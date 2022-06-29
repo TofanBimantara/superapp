98b654bb06e81ca573afe1fcca644341480e1e24
10 <10@DESKTOP-26FPGN9>
2022-06-30 06:42:43 +0700

    Test data

---
+++ Test Suites/Default menu/TS001_defaultMenu.ts
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS001_defaultMenu</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>7a4a8f2b-743d-4c0b-86b6-30cbb05ce1c4</testSuiteGuid>
+   <testCaseLink>
+      <guid>d2736794-0bd7-4fb4-b8c6-5885a869d6a6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DefaultMenu/TS003_DefaultMenu</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>21c2e3c7-28c2-4c21-b485-7a6705a9764b</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/DefaultMenu/DF001_DefaultMenu</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>21c2e3c7-28c2-4c21-b485-7a6705a9764b</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Status</value>
+         <variableId>ba77a0d8-d787-487f-8a70-b5572d4ce51a</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
